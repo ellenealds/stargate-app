e67feca152087dba3d171685000be8d48b877d70
ellenealds <107104287+ellenealds@users.noreply.github.com>
2022-10-15 14:50:14 +0100

    Update stargate.py

--- stargate.py
+++ stargate.py
@@ -39,18 +39,10 @@ def summarise(text_input):
     )
 
     return response['choices'][0]['text']
-main = "Victoria Martin"
-
-# create a multi select box
-#st.subheader('Select characters')
-#character = st.multiselect('Characters', ['Major John Sheppard','Dr. Elizabeth Weir', 'Ronon Dex', 'Colonel Steven Caldwell', 'Lt. Laura Cadman', 'Kavanagh', 'Lt. Colonel John Edwards', 'Woolsey', 'Lorne', 'Weir (Replicator)', 'Todd', 'Michael Kenmore', ' Hermiod', 'Dr. Rodney McKay', 'Teyla Emmagan', 'Lt. Aiden Ford', 'Dr. Carson Beckett', 'Sgt. Major Ronald Greer', 'Lt. Colonel Samantha Carter', 'Dr. Jennifer Keller'])
-# concatenate the selected options into a comma separated string
-#character = ', '.join(character)
 
 plot_twist = st.multiselect('Plot Twists', ['The team discovers a Stargate on a planet that is about to be destroyed by a supernova.', 'The team discovers a planet that is inhabited by humans.', 'The team discovers a planet that is inhabited by aliens.', 'The team discovers a planet that is inhabited by both humans and aliens.', 'The team discovers a planet that is inhabited by Goauld.', 'The team discovers a planet that is inhabited by Wraith.', 'The team discovers a planet that is inhabited by Ancients.', 'The team discovers a planet that is uninhabitable.', 'The team is attacked by a Goauld ship.', 'The team is attacked by a Wraith ship.', 'The team is attacked by an Ancient ship.', 'The team is stranded on a planet with no Stargate.', ' The team is attacked by a human ship.', 'The team discovers a Goauld ship.', 'The team discovers a Zpm.', 'The team discovers an Ancient ship.', 'The team discovers a Wraith ship.', 'The team discovers a planet with a Stargate that is about to be destroyed by a supernova.', 'The team is stranded on a planet with no food or water.', 'The team is stranded on a planet with no way to contact Earth.'])
 plot_twist = ', '.join(plot_twist)
 
-#text_inputinlc = st.text_input('Enter additonal information about your story, for example: include a romantic scene.')
 
 text_input = f"Generate a two-paragraph episode synopsis for Stargate Atlantis including Victoria Martin and John Sheppard as the main characters\ninclude a plot twist about:{plot_twist}\nJohn falls in love with Victoria.\n\n\n"
 #display the prompt
@@ -60,6 +52,9 @@ text_input = f"Generate a two-paragraph episode synopsis for Stargate Atlantis i
 button = st.button('Generate Episode Synopsis')
 # if the button is clicked
 if button:
-    # run the summarise function
-    summary = summarise(text_input)
-    st.write(summary)
+    with st.spinner('Generating Episode Synopsis...'):
+        summary = summarise(text_input)
+        st.write(summary)
+
+# add a footer
+st.subheader('Created with love by: Elle Neal')
